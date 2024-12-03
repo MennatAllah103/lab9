ee778cae694b1b3e3c8f49a77614de1fe6034edb
DareenMohamed11 <dareenmohm25@gmail.com>
2024-12-03 23:04:07 +0200

    Update UserDataBase.java

--- src/UserManagementBackend/UserDataBase.java
+++ src/UserManagementBackend/UserDataBase.java
@@ -60,7 +60,7 @@ public class UserDataBase {
                 String password = userJson.getString("password");
                 String username = userJson.getString("username");
                 boolean status = userJson.getBoolean("status");
-                Users.add(new User(id, email, date, password, status, username));
+                Users.add(new User( email, date, password, username));
             }
 
         } catch (IOException e) {
