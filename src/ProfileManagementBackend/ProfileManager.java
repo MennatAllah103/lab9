c5d528be31bc4fcce8202bb8a521334df447ab34
fai1205 <154233168+fai1205@users.noreply.github.com>
2024-12-05 01:41:04 +0200

    handled profile updates in backend and frontend

---
+++ src/ProfileManagementBackend/ProfileManager.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ProfileManagementBackend;
+import java.util.ArrayList;
+import UserManagementBackend
+
+/**
+ *
+ * @author hp
+ */
+public class ProfileManager {
+    
+    
+    
+    
+    
+//    private ArrayList<Post> fetchUserPosts(String userId) {
+//    ArrayList<Post> posts = new ArrayList<>();
+//    
+//    return posts;
+// }
+    
+}
